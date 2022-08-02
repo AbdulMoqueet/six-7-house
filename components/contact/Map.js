deef7289ac09268f760e002e887997a555e45c18
Abdul Moqueet <moqueetabdul@gmail.com>
2022-08-02 17:35:51 +0530

    typo corrected

--- components/contact/Map.js
+++ components/contact/Map.js
@@ -7,7 +7,7 @@ const Map = () => {
 
 
             <Box>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d712126.982088202!2d18.304357075415588!3d-33.99068014066833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc6765e329248d%3A0x395e0f731f0566e9!2sIziko%20Bo-Kaap%20Museum!5e0!3m2!1sen!2sin!4v1659429254577!5m2!1sen!2sin" width="100%" height="600px" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d712126.982088202!2d18.304357075415588!3d-33.99068014066833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc6765e329248d%3A0x395e0f731f0566e9!2sIziko%20Bo-Kaap%20Museum!5e0!3m2!1sen!2sin!4v1659429254577!5m2!1sen!2sin" width="100%" height="600px" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </Box>
 
 
